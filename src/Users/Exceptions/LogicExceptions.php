a495e46001629e31a7d8202852cd8ba979613fa5
blitzik <ales.tichava@gmail.com>
2017-04-16 16:10:18 +0200

    -

--- src/Users/Exceptions/LogicExceptions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Users\Exceptions\Logic;
-
-class LogicException extends \LogicException {}
-
-    class DomainException extends LogicException {}
-
-    class InvalidArgumentException extends LogicException {}
