c5a76f94afdb964b39095c3205ef02e799799496
blitzik <ales.tichava@gmail.com>
2017-06-11 18:31:07 +0200

    refactoring: Button that opens sidebar is smaller now + changed utf8 entity of "copy down" button. Also added jquery ui touch punch script so users on mobile devices can use sliders

--- src/Common/Presenters/AppPresenter.php
+++ src/Common/Presenters/AppPresenter.php
@@ -50,7 +50,7 @@ abstract class AppPresenter extends Presenter
     {
         parent::beforeRender();
 
-        $this->template->assetsVersion = '007';
+        $this->template->assetsVersion = '009';
     }
 
 
